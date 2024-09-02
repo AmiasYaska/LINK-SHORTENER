40ca2f20b113fcb9f3858461dafb4d1709a34c55
AmiasYaska <amiasyaska@gmail.com>
2024-09-02 09:59:12 +0300

    changes

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,4 +2,18 @@ class LinksController < ApplicationController
     def index
         @links = Link.all
     end
+
+    def create
+        @link = Link.find(link_params)
+        if @link.save
+            redirect_to :root_path
+        else
+            render :link, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def link_params
+        params.require(:link).permit(:url)
+    end
 end
