c75343b2c68745444074d013ee34d337abf67aca
AmiasYaska <amiasyaska@gmail.com>
2024-09-03 15:46:55 +0300

    changes

---
+++ app/models/base62.rb
@@ -0,0 +1,10 @@
+class Base62 < ApplicationRecord
+    ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    def encode(number)
+        solution = ALPHABET.length
+        solution
+
+    end
+
+end
