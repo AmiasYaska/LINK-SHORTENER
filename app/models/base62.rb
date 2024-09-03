9f836b46b5cedb863a1508e0ff172d975c198328
AmiasYaska <amiasyaska@gmail.com>
2024-09-03 16:51:20 +0300

    changes

--- app/models/base62.rb
+++ app/models/base62.rb
@@ -1,9 +1,26 @@
 class Base62
     ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
+    BASE = ALPHABET.length
+    
     def encode(number)
-        solution = ALPHABET.length
-        solution
+        result = ""
+        while number > 0
+          answer = number / 62
+          remainder = number % 62
+          result = ALPHABET[remainder] + result
+          number = answer
+        end
+        result
     end
+
+    def decode(string)
+        result = 0
+        string.reverse.each_char.with_index do |char, index|
+          result += ALPHABET.index(char) * (BASE ** index)
+        end
+        result
+    end
+
+
 
 end
